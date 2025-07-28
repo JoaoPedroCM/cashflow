3e26106e91e9fc48853358bdf65c9466993e4875
JoaoPedroCM <joaopedro_matias@hotmail.com>
2025-07-28 13:45:56 -0300

    feat: adiciona seeder para usuários com perfis administrador, auxiliar e funcionário

---
+++ database/seeders/UsuarioSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Usuario;
+
+class UsuarioSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Usuario::create([
+            'nome' => 'Admin Master',
+            'email' => 'admin@exemplo.com',
+            'senha' => 'master123',
+            'numero' => '999999999',
+            'endereco' => 'Rua Principal, 123',
+            'tipo_usuario' => 'administrador',
+        ]);
+
+        Usuario::create([
+            'nome' => 'Henry Santos',
+            'email' => 'henrysantos@exemplo.com',
+            'senha' => 'henry123',
+            'numero' => '999999999',
+            'endereco' => 'Rua Alameda, 123',
+            'tipo_usuario' => 'auxiliar',
+        ]);
+
+        Usuario::create([
+            'nome' => 'Rosa Andrade',
+            'email' => 'rosaandrade@exemplo.com',
+            'senha' => 'rosa123',
+            'numero' => '999999999',
+            'endereco' => 'Avenida Senador Marcos Costa, 321',
+            'tipo_usuario' => 'funcionario',
+        ]);
+    }
+}
